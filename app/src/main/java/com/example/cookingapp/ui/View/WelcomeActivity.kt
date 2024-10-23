0ddf523053fc596a6a2c395dcfed8d71320c3eff
mairanidaniele <dmairani@studenti.uninsubria.it>
2024-10-23 19:05:27 +0200

    Creazione Activity di registrazione

--- app/src/main/java/com/example/cookingapp/ui/View/WelcomeActivity.kt
+++ app/src/main/java/com/example/cookingapp/ui/View/WelcomeActivity.kt
@@ -1,6 +1,10 @@
 package com.example.cookingapp.ui.View
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +14,9 @@ import com.example.cookingapp.R
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         enableEdgeToEdge()
         setContentView(R.layout.activity_welcome)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -17,5 +24,8 @@ class WelcomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun register(view: View) {
+        startActivity(Intent(this, RegisterActivity::class.java))
     }
 }
